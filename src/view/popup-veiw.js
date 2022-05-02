dadd615a836f0a52183631d0d178f58003dd9cbc
Алина <alinascarlett@mail.ru>
2022-05-02 22:58:09 +0300

    добавляет атрибут checked

--- src/view/popup-veiw.js
+++ src/view/popup-veiw.js
@@ -49,8 +49,8 @@ const createPopupTemplate = (movie, comments) => {
 
   const commentsListTemplate = createCommentsListTemplate(movieComments);
 
-  const createCommentEmotionsTemplate = () => EMOTIONS.map((emotion) =>
-    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emotion}" value="${emotion}">
+  const createCommentEmotionsTemplate = (currentEmotion) => EMOTIONS.map((emotion) =>
+    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emotion}" value="${emotion}" ${currentEmotion === emotion ? 'checked' : ''}>
     <label class="film-details__emoji-label" for="emoji-${emotion}">
       <img src="./images/emoji/${emotion}.png" width="30" height="30" alt="emoji">
     </label>`).join('');
